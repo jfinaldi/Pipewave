8ee26fc8d81d918b2132a61f3542b62524dd9025
freshskates <business.robertcacho@gmail.com>
2021-02-01 22:00:07 -0800

    80% done with reviews

--- application/models/Review.js
+++ application/models/Review.js
@@ -10,10 +10,16 @@ Review.addReview = async (comment, postid, userid) => {
 Review.getReviews = async (postid, limit) => {
   //TODO: Maybe add limit to amount of Reviews to get on each individual post
   //TO ADD just LIMIT ? at end of SQL line
+  let getReview = "SELECT AVG(review) as avg FROM reviews WHERE fk_postid_c=?;";
+  let [r, fields] = await db.query(getReview, [postid]);
+  let avg = parseFloat(r[0].avg).toFixed(2);
+
   let baseSQL1 =
     "SELECT u.name,u.title, u.username, p.id, c.comment, c.created FROM posts p JOIN reviews c JOIN users u ON u.id=fk_userid_c ON p.id=fk_postid_c WHERE p.id=? ORDER BY created DESC";
   let [r1, fields2] = await db.query(baseSQL1, [postid]);
   console.log(r1);
+  r1.avg = avg;
   return r1;
 };
+
 module.exports = Review;
--- application/public/js/comment.js
+++ application/public/js/comment.js
@@ -1,5 +1,37 @@
 const post_review = document.getElementById("post_review");
 const leave_a_review = document.getElementById("leave_a_review");
+const review = document.getElementById("avg");
+// const star1 = document.getElementById("formstar4");
+function color(x, extra = "") {
+  for (let i = 0; i < parseInt(x); i++) {
+    document.getElementById(`${extra}star${i + 1}`).style.color =
+      "rgb(120, 209, 224)";
+  }
+}
+color(review.textContent);
+const formavg = document.getElementById("formavg");
+document.getElementById("formstar1").addEventListener("click", () => {
+  formavg.innerHTML = "1.0";
+  color(1, "form");
+});
+document.getElementById("formstar2").addEventListener("click", () => {
+  formavg.innerHTML = "2.0";
+  color(2, "form");
+});
+document.getElementById("formstar3").addEventListener("click", () => {
+  formavg.innerHTML = "3.0";
+  color(3, "form");
+});
+document.getElementById("formstar4").addEventListener("click", () => {
+  formavg.innerHTML = "4.0";
+  color(4, "form");
+});
+document.getElementById("formstar5").addEventListener("click", () => {
+  formavg.innerHTML = "5.0";
+  color(5, "form");
+});
+
+console.log(review.textContent);
 let comments_displayed = false;
 // clean code to toggle review section :)
 const display_comments = x => {
