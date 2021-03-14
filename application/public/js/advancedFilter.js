8898f9aff2114a0972283a4e6b0c4e1d83131ed1
Jennifer <jfinaldi@mail.sfsu.edu>
2021-03-14 14:24:50 -0700

    Remove Go button element when user clicks Advanced to access advanced filters

--- application/public/js/advancedFilter.js
+++ application/public/js/advancedFilter.js
@@ -1,7 +1,10 @@
 const filter = document.getElementById("filterButton");
+var goButton = document.getElementById("button");
 
 filter.addEventListener("click", e => {
     console.log("clicked");
+    goButton.remove(); // remove the other GO button
+    console.log("go button removed.");
     displayAdvancedFilter();
 })
 
