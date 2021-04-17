2afab00c9b0c6e9a4e7862fb09cdf7dd1f86fe07
freshskates <business.robertcacho@gmail.com>
2021-04-17 14:42:13 -0700

    front end input validation

--- application/public/js/home.js
+++ application/public/js/home.js
@@ -1,4 +1,20 @@
 // const counter = getElementById("counter");
 console.log("home.js connected");
+const searchbar = document.getElementById("search");
+const button = document.getElementById("button");
 
+const error_output = document.getElementById("error_output");
 
+const isLetter = str => {
+  return str.length > 0 && str.match(/^[a-z\d\-_\s]+$/i);
+};
+
+searchbar.addEventListener("input", e => {
+  if (!isLetter(e.target.value)) {
+    error_output.innerHTML = "Invalid Search Query";
+    button.disabled = true;
+  } else {
+    error_output.innerHTML = "";
+    button.disabled = false;
+  }
+});
