b1183ba97402351b7108c9ddaa2202c6b9693fb3
Kevin Danh <kevindanh21@gmail.com>
2021-04-30 18:11:06 -0700

    Fixed bug with comment.js breaking when logged into student account

--- application/public/js/comment.js
+++ application/public/js/comment.js
@@ -9,27 +9,29 @@ function color(x, extra = "") {
 }
 color(review.textContent);
 const formavg = document.getElementById("formavg");
-document.getElementById("formstar1").addEventListener("click", () => {
-  formavg.innerHTML = "1.0";
-  color(1, "form");
-});
-document.getElementById("formstar2").addEventListener("click", () => {
-  formavg.innerHTML = "2.0";
-  color(2, "form");
-});
-document.getElementById("formstar3").addEventListener("click", () => {
-  formavg.innerHTML = "3.0";
-  color(3, "form");
-});
-document.getElementById("formstar4").addEventListener("click", () => {
-  formavg.innerHTML = "4.0";
-  color(4, "form");
-});
-document.getElementById("formstar5").addEventListener("click", () => {
-  formavg.innerHTML = "5.0";
-  color(5, "form");
-});
-
+if(formavg != null)
+{
+  document.getElementById("formstar1").addEventListener("click", () => {
+    formavg.innerHTML = "1.0";
+    color(1, "form");
+  });
+  document.getElementById("formstar2").addEventListener("click", () => {
+    formavg.innerHTML = "2.0";
+    color(2, "form");
+  });
+  document.getElementById("formstar3").addEventListener("click", () => {
+    formavg.innerHTML = "3.0";
+    color(3, "form");
+  });
+  document.getElementById("formstar4").addEventListener("click", () => {
+    formavg.innerHTML = "4.0";
+    color(4, "form");
+  });
+  document.getElementById("formstar5").addEventListener("click", () => {
+    formavg.innerHTML = "5.0";
+    color(5, "form");
+  });
+}
 let comments_displayed = false;
 // clean code to toggle review section :)
 const display_comments = x => {
@@ -37,9 +39,11 @@ const display_comments = x => {
   comments_displayed = !comments_displayed;
   comments_displayed && leave_a_review.scrollIntoView({ behavior: "smooth" });
 };
-
-post_review.addEventListener("click", () => {
-  display_comments(!comments_displayed ? 1 : 0);
-});
+if(post_review != null)
+{
+  post_review.addEventListener("click", () => {
+    display_comments(!comments_displayed ? 1 : 0);
+  });
+}
 
 console.log("comment.js connected")
